fb9bfcfa08a327e647cdecff198e18f96eb1ad4d
PNISHITHAREDDY <61269708+PNISHITHAREDDY@users.noreply.github.com>
2020-04-20 16:39:22 +0530

    Add files via upload

---
+++ BookAppointment/src/main/java/com/anurag/cg/dto/Diagnosticcenter.java
@@ -0,0 +1,38 @@
+package com.anurag.cg.dto;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Diagnosticcenter 
+{
+    @Id
+    @Column(name="center_id")
+	   String centerId;
+    @Column(name="center_name")
+	   String centerName;
+     public Diagnosticcenter() {	}
+	public Diagnosticcenter(String centerId, String centerName) {
+		this.centerId = centerId;
+		this.centerName = centerName;
+		
+	}
+	
+	public String getCenterId() {
+		return centerId;
+	}
+	public void setCenterId(String centerId) {
+		this.centerId = centerId;
+	}
+	public String getCenterName() {
+		return centerName;
+	}
+	public void setCenterName(String centerName) {
+		this.centerName = centerName;
+	}
+	
+	
+    
+ 
+ 
+    
+}
